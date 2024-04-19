f55d8046007cd6f17db615500995b0890286c67f
CheechRockwizard <83024977+ChillibyteUK@users.noreply.github.com>
2024-04-19 11:02:39 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -12,9 +12,8 @@ $bg = wp_get_attachment_image_url(get_field('hero_image', 'options'), 'full');
 $homepage_id = get_option('page_on_front');
 $blocks = parse_blocks(get_post_field('post_content', $homepage_id));
 foreach ($blocks as $block) {
-    echo $block['blockName'] . '<br>';
-    if ($block['blockName'] === 'acf/cb_hero_slideshow') {
-        $images = get_field('slides', $block['attrs']['id']);
+    if ($block['blockName'] === 'acf/cb-hero-slideshow') {
+        $images = $block['attrs']['data']['slides'];
         if ($images) {
             $first_image = $images[0];
             $bg = wp_get_attachment_image_url($first_image, 'full');
@@ -25,25 +24,17 @@ foreach ($blocks as $block) {
 
 ?>
     <!-- hero -->
-    <section id="hero" class="hero d-flex align-items-center hero--default mb-0">
+    <section class="hero" style="background-image:url(<?=$bg?>)">
         <div class="overlay"></div>
-        <div class="hero__inner container-xl">
-            <div class="row h-100">
-                <div class="col-lg-6 hero__content d-flex flex-column justify-content-center order-2 order-lg-1 py-5"
-                    data-aos="fade">
-                    <h1 class="mb-4 text-start">404 - Page Not Found</h1>
-                    <div class="hero__content fs-5 mb-4">We can't seem to find the page you're looking for</div>
-                    <div class="hero__cta">
-                        <a class="btn btn-deep-green mb-4" href="/">Return to Homepage</a>
-                    </div>
-                </div>
-                <div class="col-lg-6 hero__image order-1 order-lg-2"
-                    style="background-image:url(<?=$bg?>)">
-                </div>
+        <div class="container-xl my-auto">
+            <h1 class="text-start">404 - Page Not Found</h1>
+            <div class="hero__content fs-5 mb-4 text-white">We can't seem to find the page you're looking for</div>
+            <div class="hero__cta">
+                <a class="btn btn-green-deep mb-4" href="/">Return to Homepage</a>
             </div>
         </div>
-        <div class="overlay--bottom"></div>
     </section>
+
 </main>
 <?php
 get_footer();
